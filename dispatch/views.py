422058101533dcd94d13e8d90ff321293b3445b4
Chiller <egalaczi_business@yahoo.com>
2011-11-13 18:14:14 +0100

    Added custom authentication to package resource: it falls back to django is_authenticated

--- dispatch/views.py
+++ dispatch/views.py
@@ -1,1 +1,42 @@
 # Create your views here.
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseServerError, HttpResponseForbidden, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+
+@csrf_exempt
+def loginview(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    print 'alma'
+    print username, password
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return HttpResponse("Logged in")
+        else:
+            return HttpResponseForbidden('Account disabled')
+    else:
+        return HttpResponseForbidden('Invalid login')
+
+def index(request):
+	
+    if request.user.is_authenticated():
+        print 'auth'
+
+    return HttpResponse("""
+	<html>
+	<body>
+
+	<form method="POST" action="/dispatch/login/">
+<input type="text" name="username">
+<input type="text" name="password"> 
+<input type="submit" > 
+</form>
+
+	</body>
+	</html>
+
+
+
+	""")
--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from dispatch.urls import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -18,6 +18,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
  	url(r'^api/', include(package_resource.urls)),
+ 	url(r'^dispatch/', include('dispatch.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
