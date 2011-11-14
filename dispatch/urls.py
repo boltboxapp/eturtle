64423edd5411a769d72ce19045a34fb54d14dcaf
lepi <lepi@symbler.com>
2011-11-14 18:07:07 +0100

    url refactor, base template nav

--- dispatch/urls.py
+++ dispatch/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.core.urlresolvers import reverse
+from django.views.generic.simple import redirect_to
+
 urlpatterns = patterns('',
    
- 	url(r'^login/', 'dispatch.views.loginview'),
- 	url(r'^$', 'dispatch.views.index'),
-
+    url(r'^login/', 'dispatch.views.loginview'),
+    url(r'^packages/', 'dispatch.views.loginview', name="package_list"),
+    url(r'^clients/', 'dispatch.views.loginview', name="client_list"),
+    url(r'^couriers/', 'dispatch.views.loginview', name="courier_list"),
 )
--- urls.py
+++ urls.py
@@ -1,27 +1,33 @@
 from django.conf.urls.defaults import patterns, include, url
 from dispatch.urls import *
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-import registration
+from django.core.urlresolvers import reverse
+from django.views.generic.simple import redirect_to
+from django.utils.functional import lazy
 
+reverse_lazy = lazy(reverse, unicode)
 admin.autodiscover()
 
 from dispatch.api import PackageResource
-
 package_resource = PackageResource()
-
    
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'eturtle.views.home', name='home'),
-    #url(r'^eturtle/', include('eturtle.foo.urls')),
 
+    #dispacth app urls
+    url(r'^dispatch/', include('dispatch.urls')),
+
+    #admin urls
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
- 	url(r'^api/', include(package_resource.urls)),
- 	url(r'^dispatch/', include('dispatch.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 
+    #api urls
+    url(r'^api/', include(package_resource.urls)),
+
+    #registration+auth app urls
     url(r'^accounts/', include('registration.backends.default.urls')),
+
+    #redirect to package_list(in dispatch app)
+    url(r'^$', redirect_to, {'url': reverse_lazy('package_list')}),
+
 )
