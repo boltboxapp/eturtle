2b1aa146c58378be2e0d0c0624c8e91040896d85
chiller <egalaczi_business@yahoo.com>
2011-11-25 22:19:46 +0100

    onexit alert

--- client/CourierClient/src/edu/turtle/MainActivity.java
+++ client/CourierClient/src/edu/turtle/MainActivity.java
@@ -1,11 +1,38 @@
 package edu.turtle;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends Activity{
+
+		@Override
+		public void onBackPressed() {
+			// TODO Auto-generated method stub
+			
+			
+			new AlertDialog.Builder(this)
+	        .setIcon(android.R.drawable.ic_dialog_alert)
+	        .setTitle(R.string.exit)
+	        .setMessage(R.string.wantexit)
+	        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+
+	            @Override
+	            public void onClick(DialogInterface dialog, int which) {
+
+	                //Stop the activity
+	                MainActivity.this.finish();    
+	            }
+
+	        })
+	        .setNegativeButton(R.string.cancel, null)
+	        .show();
+		
+		
+		}
 
 		Intent lsintent;
 		Intent nsintent;
