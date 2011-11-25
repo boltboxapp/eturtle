53f419b818d9bc94db8c8c2e6d080aac9449a38c
lepi <lepi@symbler.com>
2011-11-25 22:11:06 +0100

    login remember me

--- client/CourierClient/src/edu/turtle/LoginActivity.java
+++ client/CourierClient/src/edu/turtle/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.IBinder;
 
@@ -15,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -26,6 +28,7 @@ public class LoginActivity extends Activity {
  EditText txtPassword;
  Button btnLogin;
  Button btnCancel;
+ CheckBox chkRemember;
  
  private ApiService boundservice;
  private ServiceConnection sc;
@@ -64,30 +67,38 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         
+        SharedPreferences settings = getSharedPreferences("ETurtlePreferences", 0);
+        
         txtUserName=(EditText)this.findViewById(R.id.txtUname);
         txtPassword=(EditText)this.findViewById(R.id.txtPwd);
-        txtUserName.setText("roka");
-        txtPassword.setText("roka");
+        txtUserName.setText(settings.getString("username", ""));
+        txtPassword.setText(settings.getString("password", ""));
         btnLogin=(Button)this.findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new OnClickListener() {
-   
-    
-        	
-   @Override
-   public void onClick(View v) {
-    // TODO Auto-generated method stub
-    
-    authenticate(txtUserName.getText().toString(), txtPassword.getText().toString());
-    
-   }
-
-	
-   
-   	
-   
-  });     
-        
-        
+		   @Override
+		   public void onClick(View v) {
+			   
+		       SharedPreferences.Editor editor = getSharedPreferences("ETurtlePreferences", 0).edit();
+		       Boolean remember_me = chkRemember.isChecked();
+		       editor.putBoolean("remember_me",remember_me);
+		       editor.commit();
+			   
+			   if (chkRemember.isChecked()==true){
+				   editor.putString("username", txtUserName.getText().toString());
+				   editor.putString("password", txtPassword.getText().toString());
+			   } else {
+				   editor.remove("username");
+				   editor.remove("password");
+			   }
+			   
+		       editor.commit();
+			   
+			   authenticate(txtUserName.getText().toString(), txtPassword.getText().toString());
+		   }
+        }); 
+        chkRemember=(CheckBox)this.findViewById(R.id.chkRemember_me);
+        Boolean remember_me = settings.getBoolean("remember_me", false);
+        chkRemember.setChecked(remember_me);
     }
 
  	@Override
