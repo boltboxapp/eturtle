87a37b7a7a14b5225ddb5a8f32b1ab673146694d
Chiller <egalaczi_business@yahoo.com>
2011-11-26 15:25:00 +0100

    basic test for status codes at login with fixtures

--- server/dispatch/tests.py
+++ server/dispatch/tests.py
@@ -6,11 +6,25 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
-
+from django.core.urlresolvers import reverse
+from django.core.management import call_command
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    
+    def setUp(self):
+        import os
+        os.system('pwd')
+        call_command('loaddata','fixtures/initial_auth.json', interactive=True)
+        call_command('loaddata','fixtures/initial_dispatch.json', interactive=True)
+        call_command('loaddata','fixtures/initial_sites.json', interactive=True)
+        call_command('initialpermissions', interactive=True)
+
+    def test_login_status_codes(self):
+        response = self.client.post(reverse('api_login'), 
+                                { 'username':'foo', } )
+        self.assertEquals(response.status_code,400)
+        response = self.client.post(reverse('api_login'), 
+                                { 'username':'bad', 'password':'usernameandpassword', } )
+        self.assertEquals(response.status_code,401)
+        response = self.client.post(reverse('api_login'), 
+                                { 'username':'roka', 'password':'roka', } )
+        self.assertEquals(response.status_code,200)
